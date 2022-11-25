05f9ebb16e4c5db697cc3d71e93632e2e9c14a85
Antony <antony.budianto@tokopedia.com>
2022-11-25 22:51:49 +0700

    implement css import dynamic replacer

--- src/babel.spec.js
+++ src/babel.spec.js
@@ -10,64 +10,97 @@ const testGeneration = (message, code, expectedCode) => {
   assert.equal(transformedCode.trim(), expectedCode.trim(), message);
 };
 
+// testGeneration(
+//   "replace basic imports",
+//   `
+// import styled, { css } from "react-emotion";
+// `,
+//   `
+// import styled from "@emotion/styled";
+// import { css } from "@emotion/css";
+// `
+// );
+
+// testGeneration(
+//   "replace complex multiple imports",
+//   `
+// import styled, { css, keyframes } from "react-emotion";
+// `,
+//   `
+// import styled from "@emotion/styled";
+// import { css, keyframes } from "@emotion/css";
+// `
+// );
+
+// testGeneration(
+//   "replace only one styled from react-emotion",
+//   `
+// import styled from "react-emotion";
+// `,
+//   `
+// import styled from "@emotion/styled";
+// `
+// );
+
+// testGeneration(
+//   "replace only one styled from emotion",
+//   `
+// import styled from "react-emotion";
+// `,
+//   `
+// import styled from "@emotion/styled";
+// `
+// );
+
+// testGeneration(
+//   "replace only one styled from emotion - cjs",
+//   `
+// const styled = require("react-emotion");
+// `,
+//   `
+// const styled = require("@emotion/styled");
+// `
+// );
+
 testGeneration(
-  "replace basic imports",
+  "replace css import to emotion/react if used on styled - case1",
   `
 import styled, { css } from "react-emotion";
+
+const normalCss = css\`
+  color: #fff; 
+\`;
+const abnormalCss = css\`
+  color: #000; 
+\`;
+const Cmp = styled.div\`
+  \${abnormalCss}
+  color: 'pink';
+\`;
 `,
   `
+import { css as css2 } from '@emotion/react';
 import styled from "@emotion/styled";
 import { css } from "@emotion/css";
+const normalCss = css\`
+  color: #fff; 
+\`;
+const abnormalCss = css2\`
+  color: #000; 
+\`;
+const Cmp = styled.div\`
+  \${abnormalCss}
+  color: 'pink';
+\`;
 `
 );
 
-testGeneration(
-  "replace complex multiple imports",
-  `
-import styled, { css, keyframes } from "react-emotion";
-`,
-  `
-import styled from "@emotion/styled";
-import { css, keyframes } from "@emotion/css";
-`
-);
-
-testGeneration(
-  "replace only one styled from react-emotion",
-  `
-import styled from "react-emotion";
-`,
-  `
-import styled from "@emotion/styled";
-`
-);
-
-testGeneration(
-  "replace only one styled from emotion",
-  `
-import styled from "react-emotion";
-`,
-  `
-import styled from "@emotion/styled";
-`
-);
-
-testGeneration(
-  "replace only one styled from emotion - cjs",
-  `
-const styled = require("react-emotion");
-`,
-  `
-const styled = require("@emotion/styled");
-`
-);
-
-testGeneration(
-  "dont replace require without binding",
-  `
-  var _emotion = _interopRequireWildcard(require("emotion"));
-  `,
-  `
-  var _emotion = _interopRequireWildcard(require("emotion"));
-`
-);
+// testGeneration(
+//   "dont replace require without binding",
+//   `
+//   var _emotion = _interopRequireWildcard(require("emotion"));
+//   `,
+//   `
+//   var _emotion = _interopRequireWildcard(require("emotion"));
+// `
+// );
--- src/index.js
+++ src/index.js
@@ -25,6 +25,10 @@ const buildImport = templateBuilder(`
   import styled from '@emotion/styled';
 `);
 
+const buildImportEmotionReact = templateBuilder(`
+  import { css as css2 } from '@emotion/react';
+`);
+
 const REP = [
   { replacement: "@emotion/css", original: "emotion" },
   { replacement: "@emotion/css", original: "react-emotion" },
@@ -35,12 +39,73 @@ export default function visitor({ types: t }) {
     t.stringLiteral(replace(value, original, replacement));
   let root;
   let imported = false;
+  let listTags = [];
+  let MAP_STYLED = {};
+  let emotionReactImported = false;
   const importDeclaration = buildImport();
+  const emotionReactImportDeclaration = buildImportEmotionReact();
 
   return {
     visitor: {
-      Program(path) {
-        root = path;
+      Program: {
+        enter(path) {
+          root = path;
+        },
+        exit(path) {
+          /**
+           * Only rename all css which are embedded to styled component
+           * This will swap the css import from emotion/css to emotion/react
+           */
+          const res = listTags.filter((t) => MAP_STYLED[t.name]);
+          res.forEach((t) => {
+            t.path.node.declarations[0].init.tag.name = "css2";
+          });
+          if (!emotionReactImported && res.length) {
+            root.unshiftContainer("body", emotionReactImportDeclaration);
+            emotionReactImported = true;
+          }
+        },
+      },
+      VariableDeclaration(path) {
+        // if (path.node.declarations[0].id.name === "abnormalCss") {
+        //   console.log("vd1", path.node.declarations[0].id.name);
+        //   console.log("vd2", path.node.declarations[0].init.tag.name);
+        //   console.log("vd3", path.node.declarations[0]);
+        // }
+        if (
+          !path.node.declarations.length ||
+          !path.node.declarations[0].id ||
+          !path.node.declarations[0].init.tag
+        ) {
+          return;
+        }
+        const varName = path.node.declarations[0].id.name;
+        const tagName = path.node.declarations[0].init.tag.name;
+        if (tagName === "css" && path.scope.bindings.styled) {
+          /**
+           * Looks for every variables that call emotion css usage.
+           * Need to list all of them for now, since
+           * it cannot check the template literal reference from this callback,
+           * it needs Identifier callback for that
+           */
+          listTags.push({ name: varName, path });
+          console.log("emotion/css!!!", varName);
+        }
+      },
+      Identifier(path, parent) {
+        const type = path.parent.type;
+        const nodeName = path.node.name;
+        // console.log(type, nodeName);
+
+        /**
+         * Look for template literal on styled with css embed
+         */
+        if (["TemplateLiteral"].indexOf(type) !== -1) {
+          if (path.scope.bindings.styled) {
+            console.log("foundOnTemplateLiteral!!!", nodeName);
+            MAP_STYLED[nodeName] = true;
+          }
+        }
       },
       ImportDeclaration(path, state) {
         const { hasStyled, nonStyled } = getStyled(path);
