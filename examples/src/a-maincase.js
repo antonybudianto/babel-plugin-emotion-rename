3601be22ba080eb46d4f7604adb9b50d3cadb0f8
Antony <antony.budianto@tokopedia.com>
2023-01-02 10:30:33 +0700

    revert case a

--- examples/src/a-maincase.js
+++ examples/src/a-maincase.js
@@ -1,31 +1,34 @@
-// import React from "react";
-// import emot from "emotion";
+/* eslint-disable */
 
-const { css } = require("emotison");
-const React0 = require("react");
-const React1 = require("abc");
-const React2 = require("cde");
-const React3 = require("ef");
-const React4 = require("ax");
+// import { cx } from "@emotion/css";
+// import styled from "@emotion/styled";
+// import { css } from "@emotion/react";
 
-const abc = () => {};
-const def = () => {};
-const zoo = () => {};
+/**
+ * Should be @emotion/css + @emotion/styled + @emotion/react
+ */
+import styled, { css, cx } from "react-emotion";
 
-zoo();
-def();
-abc();
-abc();
-abc();
-abc();
-abc();
-abc();
-abc();
-abc();
-abc();
-abc();
-abc();
-abc();
-abc();
-abc();
-abc();
+/**
+ * Should be kept as is, should be `css`!!!!
+ */
+const calonPure = css("background: gray;");
+
+/**
+ * Inside anonymous fn also should be css2!
+ */
+const calonSyt = css("background: pink;");
+const Base = function Base(props) {
+  return css(props.size, "style", calonSyt);
+};
+
+/**
+ * All should output css2!!!
+ */
+const calon7Syt = css("background: blue;");
+const calon9Syt = css("background: pink;", calon7Syt);
+const calon3Syt = css("background: pink;", calon9Syt);
+const calon2Syt = css("background: blue;");
+export const DummyStyled = styled("div", {
+  target: "evxom8o1",
+})(Base, calon2Syt, calon3Syt, ";> span{outline:none;}");
